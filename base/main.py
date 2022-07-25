10ba05f164219435163f029599895049b5e1e2fb
Tony Liang <chunqingliang@gmail.com>
2022-07-25 16:51:54 -0700

    refactor bundle

--- base/main.py
+++ base/main.py
@@ -1,23 +1,17 @@
-#from base.detector.ml_bundle import MLBundle
-#from base.detector.trainer import Trainer
+from base.detector.ml_bundle import MLBundle
+from base.detector.trainer import Trainer
 from argparse import ArgumentParser
-from base.utils import MLScriptParser
+#from base.utils import MLScriptParser
 
-ourParse = MLScriptParser(ArgumentParser)
-
-# parser = ArgumentParser()
-# parser.add_argument("-b", "--bundle")
-# args = vars(parser.parse_args())
-BUNDLE_NAME = 0 #args["bundle"]
+parser = ArgumentParser()
+parser.add_argument("-b", "--bundle")
+args = vars(parser.parse_args())
+BUNDLE_NAME = args["bundle"]
 
 def main(BUNDLE_NAME):
-    print(BUNDLE_NAME)
-    print("FIX THIS LATER !!! passing arguments like name of bundle , and config")
-    #print(ourParse)
-    #print(BUNDLE_NAME)
-    #mlb = MLBundle(BUNDLE_NAME)
-    #trainer = Trainer(mlb)
-    #print(mlb.config)
+    mlb = MLBundle(BUNDLE_NAME)
+    trainer = Trainer(mlb)
+    print(trainer.config)
 
 if __name__ == "__main__":
     main(BUNDLE_NAME)
